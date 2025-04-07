83bbfcd5c4e0321b2db9d7baa2ae88b00454c93c
Abdi Dabala <abdid.yadata@gmail.com>
2025-04-07 03:20:49 -0700

    fist

---
+++ Largest Divisible Subset.py
@@ -0,0 +1,54 @@
+
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        n = len(nums)
+        f = [1] * n
+        k = 0
+        for i in range(n):
+            for j in range(i):
+                if nums[i] % nums[j] == 0:
+                    f[i] = max(f[i], f[j] + 1)
+            if f[k] < f[i]:
+                k = i
+        m = f[k]
+        i = k
+        ans = []
+        while m:
+            if nums[k] % nums[i] == 0 and f[i] == m:
+                ans.append(nums[i])
+                k, m = i, m - 1
+            i -= 1
+        return ans
+'''
+from typing import List
+
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
+
+        nums.sort()
+        n = len(nums)
+        dp = [1] * n         # dp[i]: size of the largest divisible subset ending at index i
+        prev = [-1] * n      # prev[i]: previous index in the subset ending at i
+        max_index = 0        # index of the last element in the largest subset
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] % nums[j] == 0:
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j] + 1
+                        prev[i] = j
+            if dp[i] > dp[max_index]:
+                max_index = i
+
+        # Reconstruct the largest divisible subset
+        result = []
+        while max_index != -1:
+            result.append(nums[max_index])
+            max_index = prev[max_index]
+
+        return result[::-1]  # Reverse to get correct order
+
+'''
