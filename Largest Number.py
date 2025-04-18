18cc4902c791beb7b5870acec872bf22dec8c8eb
Abdi Dabala <abdid.yadata@gmail.com>
2025-04-18 09:14:51 -0700

    Largest Number

--- Largest Number.py
+++ Largest Number.py
@@ -16,7 +16,7 @@ class Solution:
         else:
             return '0'  # If it is '0', return '0'
 
-# Example usage:
+# Example usage :
 solution = Solution()
 print(solution.largestNumber([10, 2]))  # Output: "210"
 print(solution.largestNumber([3, 30, 34, 5, 9]))  # Output: "9534330"
