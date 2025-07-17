940f5ba7c2eb2855052fccde845d334d947b7973
Abdi Dabala <abdid.yadata@gmail.com>
2025-07-17 03:04:51 -0700

    Plus One

--- Plus One.py
+++ Plus One.py
@@ -11,3 +11,9 @@ class Solution(object):
                 return digits
             digits[i] = 0
         return [1] + digits
+
+# Time and Space Complexity
+
+# Time Complexity: O(n) – In the worst case, all digits are 9 and we must process each digit.
+
+#Space Complexity: O(1) extra space (excluding the output list), since we modify the list in place (except in the all-9s case, where we create a new list).
