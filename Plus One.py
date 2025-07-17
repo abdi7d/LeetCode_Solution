31ea5211f8cf7869af79e5f71a831d2ff666c64f
Abdi Dabala <abdid.yadata@gmail.com>
2025-07-17 02:25:48 -0700

    plus one

--- Plus One.py
+++ Plus One.py
@@ -5,12 +5,9 @@ class Solution(object):
         :rtype: List[int]
         """
         n = len(digits)
-        # Start from the last digit
         for i in range(n - 1, -1, -1):
             if digits[i] < 9:
                 digits[i] += 1
                 return digits
             digits[i] = 0
-        
-        # If all digits were 9, we need an extra digit
         return [1] + digits
