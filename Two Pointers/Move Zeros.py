0762c3465b676ae92548b2020e58ca0e27375348
Abdi Dabala <abdid.yadata@gmail.com>
2025-07-23 21:21:08 -0700

    Move Zeros

--- Two Pointers/Move Zeros.py
+++ Two Pointers/Move Zeros.py
@@ -1,14 +1,11 @@
 class Solution:
-    def moveZeroes(self, nums):
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+    def moveZeroes(self, nums: List[int]) -> None:
         # First pointer: Pointer to place the next non-zero element(position to place the next non-zero element)
         last_non_zero = 0
 
-        # # Second pointer: scan the list (Move non-zeroes forward)
+        # Second pointer: scan the list (Move non-zeroes forward)
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[last_non_zero] = nums[i]
-                nums[i] = nums[last_non_zero]
+                # Swap current non-zero with last_non_zero
+                nums[last_non_zero], nums[i] = nums[i], nums[last_non_zero]
                 last_non_zero += 1
