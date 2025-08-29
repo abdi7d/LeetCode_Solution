8a88f7f2ff6076dd7daa309cff988ba829d12f83
Abdi Dabala <abdid.yadata@gmail.com>
2025-08-29 05:25:33 -0700

    Find the Peeks

---
+++ Array/2951. Find the Peaks.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findPeaks(self, mountain: List[int]) -> List[int]:
+
+        result = []
+        first_element =0
+        last_element = len(mountain)-1
+
+        for i in range(len(mountain)):
+            if i ==first_element or i ==last_element:
+                continue
+            else:
+                prev= i-1
+                next= i+1
+                if  (mountain[prev] < mountain[i]) and (mountain[next] < mountain[i]):
+                    result.append(i)
+        return result
