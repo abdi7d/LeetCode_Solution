db626d89257e6e3d192db89cfd193d8e3796cbbc
Abdi Dabala <abdid.yadata@gmail.com>
2025-09-05 07:52:12 -0700

    Find a Peak Element II

--- Array/Find a Peak Element II.py
+++ Array/Find a Peak Element II.py
@@ -1,41 +1,26 @@
 class Solution:
-    def findPeakGrid(self, rat: list[list[int]]) -> list[int]:
-        result = []
- 
-        m = len(rat)
-        for r in rat:
-            n = len(rat[m-1])
-            left = 0
-            right =n-1
-            top = 0
-            buttor =m-1
-            # out perireter with the value -1
-            # if rat
-            for c in r:
-                # corpare with left elerent
-                while (left<right) and (top<buttor):
-                    if (r[c]>r[left]) and (r[c]>r[right]):
-                        if (r[c]>r[top]) and (r[c]>r[buttor]):
-                            result= [r,c]
-                        else:
-                            left +=1
-                            top +=1
-        return result
-
-
-mat = [[1,4], [3,2]]
-abdi = Solution()
-abdi.findPeakGrid(mat)
-# rat = [[1, 4], [3, 2, 3], [4, 5]]
-
-'''  
-# print(len(rat)) # this reans how rany rows this ratrix have --- 3
-kana jechuun rat kan jedharu kun row reeqa qabaa jechuudha.
-'''
-
-
-
-'''
-# print(len(rat[1])) # row'n larraffaaan elerent reeqa qabaa ---- 3
-kana jechuun row'n kan index'n isaa 1 ta'e elerent reeqa qabaa jechuudha.
-'''
+    def findPeakGrid(self, mat: list[list[int]]) -> list[int]:
+        m = len(mat) # number of rows 
+        n = len(mat[0]) # number of columns
+        
+        def findPeakInRow(row):
+            left, right = 0, n - 1
+            while left < right:
+                mid = (left + right) // 2
+                if mat[row][mid] > mat[row][mid + 1]:
+                    right = mid
+                else:
+                    left = mid + 1
+            return left
+        
+        left, right = 0, m - 1
+        while left < right:
+            mid = (left + right) // 2
+            peak_col = findPeakInRow(mid)
+            if mat[mid][peak_col] > mat[mid + 1][peak_col]:
+                right = mid
+            else:
+                left = mid + 1
+        
+        peak_col = findPeakInRow(left)
+        return [left, peak_col]
