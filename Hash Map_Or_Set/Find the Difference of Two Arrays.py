4732e0c5e133f48acdcef481cb7fbbfb98d3ed58
Abdi Dabala <abdid.yadata@gmail.com>
2025-09-07 21:00:55 -0700

    Find the Difference of Two Arrays

--- Hash Map_Or_Set/Find the Difference of Two Arrays.py
+++ Hash Map_Or_Set/Find the Difference of Two Arrays.py
@@ -1,19 +1,27 @@
 class Solution:
-    def findDifference(self, nums1: list[int], nums2: list[int]) -> list[list[int]]:
-        answer = []
-        for i in nums1:
-            for j in nums2:
-                if nums1[i] !=nums2[j]:
-                    answer.append(nums1[i])
-                else:
-                    continue
-        for k in nums2:
-            for l  in nums1:
-                if nums1[k] !=nums2[l]:
-                    answer.append(nums1[k])
-                else:
-                    continue
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        # Set-based solution
+        set1, set2 = set(nums1), set(nums2)
+        # return [list(set1 - set2), list(set2 - set1)]
+        return [list(set1.difference(set2)), list(set2.difference(set1))]
 
-nums1 = [1, 2, 3]
-nums2 = [2, 4, 6]
+        # Time Complexity: O(n + m)
+        # Space Complexity: O(n + m)
+        
+"""
+# Manual traversal 
+        # res1, res2 = [], []
+    
+        # for x in nums1:
+        #     if x not in nums2 and x not in res1:
+        #         res1.append(x)
+        
+        # for y in nums2:
+        #     if y not in nums1 and y not in res2:
+        #         res2.append(y)
+        
+        # return [res1, res2]
 
+# Time Complexity: O(n·m + n² + m²)
+# Space Complexity: O(n + m)
+"""
