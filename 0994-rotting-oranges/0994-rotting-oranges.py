413b4a11d629ac9bbd982059ff8e964162c4280f
Abdi Dabala <abdid.yadata@gmail.com>
2026-03-25 20:18:46 +0330

    Time: 1 ms (85.03%), Space: 19.2 MB (97.97%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,33 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        queue = deque()
+        fresh = 0
+        
+        # Step 1: initialize
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == 2:
+                    queue.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh += 1
+        
+        # Step 2: BFS
+        minutes = 0
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        
+        while queue and fresh > 0:
+            for _ in range(len(queue)):
+                r, c = queue.popleft()
+                
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    
+                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        fresh -= 1
+                        queue.append((nr, nc))
+            
+            minutes += 1
+        
+        return minutes if fresh == 0 else -1
