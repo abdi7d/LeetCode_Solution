b734356633d5aa57fbc966e685c48c9e6858eddb
Abdi Dabala <abdid.yadata@gmail.com>
2026-04-09 20:30:15 +0300

    Time: 15 ms (76.22%), Space: 19.6 MB (49.29%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        count = Counter(magazine)
+        
+        for ch in ransomNote:
+            if count[ch] <= 0:
+                return False
+            count[ch] -= 1
+        
+        return True
