967f265aa2bfdf613336e4eca826cd62c584501d
Abdi Dabala <abdid.yadata@gmail.com>
2026-05-15 22:47:12 +0300

    Time: 3429 ms (73.08%), Space: 19.2 MB (94.87%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,40 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        
+        def dfs(r, c, index):
+            
+            # matched entire word
+            if index == len(word):
+                return True
+            
+            # invalid cases
+            if (
+                r < 0 or c < 0 or
+                r >= rows or c >= cols or
+                board[r][c] != word[index]
+            ):
+                return False
+            
+            # mark visited
+            temp = board[r][c]
+            board[r][c] = '#'
+            
+            found = (
+                dfs(r + 1, c, index + 1) or
+                dfs(r - 1, c, index + 1) or
+                dfs(r, c + 1, index + 1) or
+                dfs(r, c - 1, index + 1)
+            )
+            
+            # restore
+            board[r][c] = temp
+            
+            return found
+        
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+        
+        return False
