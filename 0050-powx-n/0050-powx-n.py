d592479b1493ffa7ed6411fb51977aec09f67821
Abdi Dabala <abdid.yadata@gmail.com>
2026-05-16 01:36:25 +0300

    Time: 0 ms (100%), Space: 19.5 MB (19.65%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,20 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        # handle negative exponent
+        if n < 0:
+            x = 1 / x
+            n = -n
+        
+        result = 1
+        
+        while n > 0:
+            
+            # if n is odd
+            if n % 2 == 1:
+                result *= x
+            
+            x *= x
+            n //= 2
+        
+        return result
